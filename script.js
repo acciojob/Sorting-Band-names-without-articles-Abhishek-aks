3e26a84b488081e793d1ea849019f37c98e78108
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 01:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listItem=document.getElementById("list");
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 touristSpots.sort();
-
+// console.log(touristSpots);
 touristSpots.forEach((item)=>{
 	const list=document.createElement("li");
 	list.innerText=item;
