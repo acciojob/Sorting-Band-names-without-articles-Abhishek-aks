a2efba15c1c25afa4d0de2359c7badf4ae98c2a6
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 01:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-//your code here
-const listItem=document.getElementById("list");
-
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
-touristSpots.sort();
-// console.log(touristSpots);
-touristSpots.forEach((item)=>{
-	const list=document.createElement("li");
-	list.innerText=item;
-
-	listItem.appendChild(list);
-})
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
+}
+}
+return s.trim()
+}
